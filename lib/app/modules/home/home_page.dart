1548d30f1510db3b3c5dae02b26e473e16b70c59
Cirrus CI <support@cirruslabs.org>
2020-10-08 10:53:49 +0000

    Card dos pokemons

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -11,23 +11,23 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends ModularState<HomePage, HomeController> {
   List<Pokemon> pokemons = [
     Pokemon(
-        id: 'xx',
-        imageUrl: '',
-        imageUrl1HiRes: 'xx',
-        name: 'Magneton',
+        id: "neo4",
+        name: "Neo Destiny",
+        imageUrl: "https://images.pokemontcg.io/neo4/symbol.png",
+        imageUrl1HiRes: "https://images.pokemontcg.io/neo4/logo.png",
         types: ["Lightning"]),
     Pokemon(
-        id: 'xy',
-        imageUrl: '',
-        imageUrl1HiRes: 'xy',
-        name: 'Alakazan',
-        types: ["Psychic"]),
+        id: "neo3",
+        name: "Neo Revelation",
+        imageUrl: "https://images.pokemontcg.io/neo3/symbol.png",
+        imageUrl1HiRes: "https://images.pokemontcg.io/neo3/logo.png",
+        types: ["Lightning"]),
     Pokemon(
-        id: 'xz',
-        imageUrl: '',
-        imageUrl1HiRes: 'xz',
-        name: 'Fighting',
-        types: ["Hariyama"]),
+        id: "si1",
+        name: "Southern Islands",
+        imageUrl: "https://images.pokemontcg.io/si1/symbol.png",
+        imageUrl1HiRes: "https://images.pokemontcg.io/si1/logo.png",
+        types: ["Lightning"]),
   ];
 
   @override
@@ -38,15 +38,44 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
         crossAxisCount: 2,
         children: List.generate(pokemons.length, (index) {
           var poke = pokemons[index];
-          return Padding(
-            padding: const EdgeInsets.only(bottom: 5.0),
-            child: Hero(
-              tag: poke.id,
-              child: Image.network(poke.imageUrl),
+          return GestureDetector(
+            onTap: () => detalharPokemon(poke),
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 5.0),
+              child: Hero(
+                tag: poke.id,
+                child: Image.network(poke.imageUrl),
+              ),
             ),
           );
         }),
       ),
+    );
+  }
+
+  detalharPokemon(Pokemon poke) {
+    Navigator.of(context).push(
+        PageRouteBuilder(pageBuilder: (context, animation, secondaryAnimation) {
+      return PaginaDetalhe(poke: poke);
+    }));
+  }
+}
+
+class PaginaDetalhe extends StatefulWidget {
+  final Pokemon poke;
+
+  PaginaDetalhe({this.poke});
+
+  @override
+  _PaginaDetalheState createState() => _PaginaDetalheState();
+}
+
+class _PaginaDetalheState extends State<PaginaDetalhe> {
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: widget.poke.id,
+      child: Image.network(widget.poke.imageUrl),
     );
   }
 }
